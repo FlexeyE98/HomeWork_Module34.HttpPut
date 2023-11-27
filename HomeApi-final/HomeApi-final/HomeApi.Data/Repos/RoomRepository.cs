4fdff7f95e4de3668bd6da2b1cd4d7516ab4c3a1
Alexey Emelchikov <aleksemelchikov@gmail.com>
2023-11-28 00:39:12 +0300

    Реализация метода PUT

---
+++ HomeApi-final/HomeApi-final/HomeApi.Data/Repos/RoomRepository.cs
@@ -0,0 +1,63 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using HomeApi.Data.Models;
+using HomeApi.Data.Queries;
+using Microsoft.EntityFrameworkCore;
+
+namespace HomeApi.Data.Repos
+{
+    /// <summary>
+    /// Репозиторий для операций с объектами типа "Room" в базе
+    /// </summary>
+    public class RoomRepository : IRoomRepository
+    {
+        private readonly HomeApiContext _context;
+
+        public RoomRepository(HomeApiContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        ///  Найти комнату по имени
+        /// </summary>
+        public async Task<Room> GetRoomByName(string name)
+        {
+            return await _context.Rooms.Where(r => r.Name == name).FirstOrDefaultAsync();
+        }
+
+        /// <summary>
+        ///  Добавить новую комнату
+        /// </summary>
+        public async Task AddRoom(Room room)
+        {
+            var entry = _context.Entry(room);
+            if (entry.State == EntityState.Detached)
+                await _context.Rooms.AddAsync(room);
+
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateRoom(Room room, UpdateRoomQuery roomQuery)
+        {
+            room.GasConnected = roomQuery.NewGasConnected;
+
+            if(!string.IsNullOrEmpty(roomQuery.NewName))
+                room.Name = roomQuery.NewName;
+            if(roomQuery.NewArea != 0)
+                room.Area = roomQuery.NewArea;
+            if(roomQuery.NewVoltage != 0)
+                room.Voltage = roomQuery.NewVoltage;
+
+            var entry = _context.Entry(room);
+            if (entry.State == EntityState.Detached)
+                _context.Rooms.Update(room);
+
+            await _context.SaveChangesAsync();
+
+
+
+
+        }
+    }
+}
